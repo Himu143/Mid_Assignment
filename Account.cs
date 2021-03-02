74066959f37423036e446dc82dc76870587fe1d1
Himu143 <78273243+Himu143@users.noreply.github.com>
2021-03-02 13:46:45 +0600

    add account class

---
+++ Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mid_Assignment
+{
+    class Account
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+    }
+}
